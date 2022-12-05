e63454e4eca5f6dffa842c437de580d9d2615e03
Robertleoj <robert.leo.jonsson@gmail.com>
2022-12-05 15:48:58 +0000

    get ready for cloud training

--- src/2p5D/train.py
+++ src/2p5D/train.py
@@ -2,6 +2,7 @@ from datasets import Data
 from load_config import load_config
 from utils import model_from_config
 from trainer.trainer import Trainer
+from sys import argv
 
 config = load_config()
 
@@ -14,4 +15,6 @@ net = model_from_config(config)
 
 trainer = Trainer(config=config, net=net, train_set=train_set, val_set=val_set)
 
-trainer.train(1)
+n_epochs = 5 if len(argv) == 1 else int(argv[1])
+
+trainer.train(n_epochs)
